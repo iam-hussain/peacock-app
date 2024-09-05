bf47ddb254feb796ef5a3be0c5dac56f2cbe22af
Hussain <itsjakirhussain@gmail.com>
2024-09-05 22:39:14 +0530

    ReactQueryDevtools

--- src/components/providers/query-provider.tsx
+++ src/components/providers/query-provider.tsx
@@ -1,29 +1,17 @@
-"use client";
+'use client'
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode } from "react"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: true,
-      refetchOnMount: true,
-      refetchOnReconnect: true,
-      retry: 1,
-      staleTime: Number.POSITIVE_INFINITY,
-    },
-  },
-});
-
-export default function QueryProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+function QueryProvider({ children }: { children: ReactNode }) {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false}/>
+        </QueryClientProvider>
+    )
 }
+
+export  default QueryProvider
